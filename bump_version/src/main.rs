ec45018d27c67ea5d4c6164eed0b8052e3c78091
Tobias Bieniek <tobias@bieniek.cloud>
2023-11-13 07:40:35 +0100

    GitHub Actions: Add `update` workflow

--- bump_version/src/main.rs
+++ bump_version/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 fn main() {
     // Find version field in Cargo.toml
     let cargo_toml = std::fs::read_to_string("Cargo.toml").expect("Failed to read Cargo.toml");
@@ -22,4 +24,14 @@ fn main() {
         format!(r#"version = "{}""#, new_version).as_str(),
     );
     std::fs::write("Cargo.toml", new_cargo_toml.as_bytes()).expect("Failed to write Cargo.toml");
+
+    // If the `GITHUB_OUTPUT` env var is set, append the new version to the file
+    if let Ok(path) = std::env::var("GITHUB_OUTPUT") {
+        let mut file = std::fs::OpenOptions::new()
+            .append(true)
+            .open(path)
+            .expect("Failed to open $GITHUB_OUTPUT");
+
+        writeln!(file, "version={new_version}").expect("Failed to write to  $GITHUB_OUTPUT");
+    }
 }
