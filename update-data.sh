edbe48e9f46cd95cc8a4a6a7f842ada53b7433d9
Tobias Bieniek <tobias@bieniek.cloud>
2023-11-13 09:44:08 +0100

    Use `jq` to pretty-print downloaded JSON files

--- update-data.sh
+++ update-data.sh
@@ -2,5 +2,11 @@
 set -euxo pipefail
 
 mkdir -p data
+
 curl https://api.github.com/meta --output data/meta.json
+jq --monochrome-output '.' data/meta.json > data/meta.json.tmp
+mv data/meta.json.tmp data/meta.json
+
 curl https://api.github.com/meta/public_keys/secret_scanning --output data/secret-scanning.json
+jq --monochrome-output '.' data/secret-scanning.json > data/secret-scanning.json.tmp
+mv data/secret-scanning.json.tmp data/secret-scanning.json
